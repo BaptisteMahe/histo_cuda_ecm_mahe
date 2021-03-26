2e2f3390f498f14f288f88b8666e40f60441165a
BaptisteMahe <mahe.baptiste.19@gmail.com>
2021-03-26 14:14:11 +0100

    Fix error

--- template.cu
+++ template.cu
@@ -62,7 +62,7 @@ void kernelFunction(char* d_data, int* d_result, int nbLine, size_t pitch) {
 		int currentLetter = line[index];
 
 		while (currentLetter > 0) {
-	    	atomicAdd(s_result[currentLetter], 1);
+	    	atomicAdd(&s_result[currentLetter], 1);
 	    	index++;
 	    	currentLetter = line[index];
 		}
@@ -71,6 +71,7 @@ void kernelFunction(char* d_data, int* d_result, int nbLine, size_t pitch) {
 
         if (tidb == 0) {
             for (int index = 0; index < NB_ASCII; index++) {
+                printf("%i", s_result[index]);
                 atomicAdd(&d_result[index], s_result[index]);
             }
         }
