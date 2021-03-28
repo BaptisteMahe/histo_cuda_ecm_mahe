34d0569d7ef1a0c048df21fbbcce948b36e4848b
BaptisteMahe <mahe.baptiste.19@gmail.com>
2021-03-28 01:24:24 +0100

    Try reset shared memory to 0

--- template.cu
+++ template.cu
@@ -55,6 +55,11 @@ void kernelFunction(char* d_data, int* d_result, int nbLine, size_t pitch) {
     const unsigned int ti = blockIdx.x*blockDim.x + tidb;
 
     __shared__ int s_result[NB_ASCII];
+    for (int i = 0; i < NB_ASCII; i++) {
+        s_result[i] = 0;
+    }
+
+    __syncthreads();
     
     if (ti < nbLine) {
 		char* line = (char *)((char*)d_data + ti * pitch);
